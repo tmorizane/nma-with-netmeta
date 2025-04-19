ac3d10ff2faf331182f08814b8a00d5f48fb3aa7
tmorizane <43833904+tmorizane@users.noreply.github.com>
2025-04-19 09:43:22 +0900

    Update to-nma-with-netmeta.R

--- to-nma-with-netmeta.R
+++ to-nma-with-netmeta.R
@@ -1,1 +1,5 @@
+#Windows
 dat=read.delim("clipboard",sep="\t",header=TRUE);source("https://stat.zanet.biz/useRs/scripts/nma-with-netmeta.R")
+
+#Mac
+dat=read.delim(pipe("pbpaste"),sep="\t",header=TRUE);source("https://stat.zanet.biz/useRs/scripts/nma-with-netmeta.R")
